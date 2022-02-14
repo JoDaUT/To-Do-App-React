5d7aadbc2f42748d8d4c487891d698beb3b76ad0
JoDaUT <jose.david.urena.torres@gmail.com>
2022-02-14 13:37:26 -0600

    ToDo counter implementation

--- src/components/ToDoCounter/ToDoCounter.js
+++ src/components/ToDoCounter/ToDoCounter.js
@@ -1,5 +1,11 @@
 import React from "react";
-
-export default function ToDoCounter() {
-	return <div className="toDoCounter">ToDoCounter</div>;
+import "./ToDoCounter.css";
+export default function ToDoCounter(props) {
+	const toDosCompleted = 3;
+	const totalOfToDos = 10;
+	return (
+		<h1 className="ToDoCounter">
+			Has completado {toDosCompleted} de {totalOfToDos}
+		</h1>
+	);
 }
