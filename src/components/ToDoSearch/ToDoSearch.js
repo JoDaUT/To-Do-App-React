e2218420d88cc4038ee9dccc7c548b6cf61807da
JoDaUT <jose.david.urena.torres@gmail.com>
2022-02-14 13:38:34 -0600

    ToDo Search Component

--- src/components/ToDoSearch/ToDoSearch.js
+++ src/components/ToDoSearch/ToDoSearch.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Grid from "@mui/material/Grid";
+
 function ToDoSearch() {
-	return <div className="toDoSearch">ToDoSearch</div>;
+	return (
+		<Grid container justifyContent="center">
+			<InputBase
+				placeholder="Buscar to-do"
+				inputProps={{ "aria-label": "Buscar to-do" }}
+			/>
+			<IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+				<SearchIcon />
+			</IconButton>
+		</Grid>
+	);
 }
 
 export default ToDoSearch;
