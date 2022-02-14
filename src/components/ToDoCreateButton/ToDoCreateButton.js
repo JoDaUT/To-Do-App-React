7d47e98e2f55f40cfb1135ff05b51d2aef70caed
JoDaUT <jose.david.urena.torres@gmail.com>
2022-02-14 13:37:56 -0600

    Add create button

--- src/components/ToDoCreateButton/ToDoCreateButton.js
+++ src/components/ToDoCreateButton/ToDoCreateButton.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+
+import "./ToDoCreateButton.css";
+
 function ToDoCreateButton() {
-	return <div className="toDoCreateButton">ToDoCreateButton</div>;
+	return (
+		<div className="ToDoCreateButton">
+			<Fab color="primary" aria-label="add">
+				<AddIcon />
+			</Fab>
+		</div>
+	);
 }
 
 export default ToDoCreateButton;
