726936020bfcdaa4b03bcf55d44e28d50352fbe3
JoDaUT <jose.david.urena.torres@gmail.com>
2022-02-13 23:01:31 -0600

    Style ToDoItems, add delete and complete buttons

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -1,13 +1,41 @@
 import React from "react";
+import "./ToDoItem.css";
+import { AiFillCheckCircle } from "react-icons/ai";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const deleteToDoItem = (e) => {
+	console.log("delete toDo");
+};
+
+const completeToDoItem = (e) => {
+	console.log("complete toDo");
+};
 
 function ToDoItem(props) {
 	const toDo = props.toDo;
 	return (
-		<div className="toDoItem">
-			<hr></hr>
-			<p>Id: {toDo.id}</p>
-			<p>Content: {toDo.content}</p>
-			<p>Created at: {toDo.createdAt.toDateString()}</p>
+		<div className="ToDoItem">
+			<div className="ToDoItem__Content">
+				<p>
+					<strong>{toDo.content}</strong>
+				</p>
+				<p>Id: {toDo.id}</p>
+				<p>Created at: {toDo.createdAt.toDateString()}</p>
+			</div>
+			<div className="ToDoItem__ButtonGroup">
+				<AiFillCheckCircle
+					color={"green"}
+					size={"35px"}
+					cursor={"pointer"}
+					onClick={completeToDoItem}
+				></AiFillCheckCircle>
+				<AiFillCloseCircle
+					color={"red"}
+					size={"35px"}
+					cursor={"pointer"}
+					onClick={deleteToDoItem}
+				></AiFillCloseCircle>
+			</div>
 		</div>
 	);
 }
--- src/services/posts.js
+++ src/services/posts.js
@@ -7,33 +7,39 @@ export const getToDoList = () => {
 			content:
 				"Magna exercitation laborum qui non proident est nostrud tempor eu quis.",
 			createdAt: new Date(),
+			completed: false,
 		},
 		{
 			id: genUUID(),
 			content: "Pariatur amet ullamco ut officia excepteur enim sit laborum.",
 			createdAt: new Date(),
+			completed: false,
 		},
 		{
 			id: genUUID(),
 			content:
 				"Duis sit minim veniam laborum qui aliquip ipsum nulla sit voluptate.",
 			createdAt: new Date(),
+			completed: false,
 		},
 		{
 			id: genUUID(),
 			content:
 				"Eiusmod id nisi culpa dolore in commodo excepteur quis non non magna laborum.",
 			createdAt: new Date(),
+			completed: false,
 		},
 		{
 			id: genUUID(),
 			content: "Cupidatat laboris ea amet reprehenderit esse quis.",
 			createdAt: new Date(),
+			completed: false,
 		},
 		{
 			id: genUUID(),
 			content: "Culpa est dolor exercitation Lorem.",
 			createdAt: new Date(),
+			completed: false,
 		},
 	];
 };
