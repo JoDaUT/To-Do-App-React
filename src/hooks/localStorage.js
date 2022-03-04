ec28e8d22d0a1744c2b023d1ff1752aaaac17bce
JoDaUT <jose.david.urena.torres@gmail.com>
2022-03-04 13:33:40 -0600

    [Add] local storage hook

---
+++ src/hooks/localStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+export function useLocalStorage(key, defaultValue) {
+	if (!localStorage) {
+		setItem(null);
+		throw Error("Local Storage not supported");
+	}
+
+	const [item, setItem] = useState(defaultValue);
+
+	useEffect(() => {
+		const localStorageItem = localStorage.getItem(key);
+		if (!localStorageItem) {
+			const serializedValue = JSON.stringify(defaultValue);
+			localStorage.setItem(key, serializedValue);
+		} else {
+			const deserializedValue = JSON.parse(localStorageItem);
+			setItem(deserializedValue);
+		}
+	}, []);
+
+	const saveItem = (item) => {
+		const serializedValue = JSON.stringify(item);
+		localStorage.setItem(key, serializedValue);
+		setItem(item);
+	};
+
+	return [item, saveItem];
+}
